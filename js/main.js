1c9c5a43130bf624a0d2c698c49273edd7ddc284
Suja Tanvir Ahmed <30548517+SujaTanvirAhmed@users.noreply.github.com>
2022-04-24 16:54:30 +0600

    added mobile sliding menu animation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const mobileMenuCont = document.querySelector(".mobile-menu-cont");
 const hamburger = document.querySelector(".hamburger");
+
 hamburger.addEventListener("click", () => {
+    let pos = -240;
+    const intervalId = setInterval(() => {
+        if (pos === 0) {
+            clearInterval(intervalId);
+        }
+        else {
+            pos = pos + 10;
+            mobileMenuCont.style.right = pos + "px";
+        }
+    }, 1);
     mobileMenu.style.display = "block";
-    mobileMenuCont.classList.add("menu-come");
 });
+
 mobileMenu.addEventListener("click", () => {
-    mobileMenuCont.classList.remove("menu-come");
-    mobileMenu.style.display = "none";
+    let pos = 0;
+    const intervalId = setInterval(() => {
+        if (pos === -240) {
+            clearInterval(intervalId);
+        }
+        else {
+            pos = pos - 10;
+            mobileMenuCont.style.right = pos + "px";
+        }
+    }, 1);
+    setTimeout(() => {
+        mobileMenu.style.display = "none";
+    }, 500);
+
 });
